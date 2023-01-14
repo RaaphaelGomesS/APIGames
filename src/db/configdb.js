127955ff2976e57114e10f34bf31a5dfab1b9f7d
Raphael Gomes Prazeres <rafagomes1706@gmail.com>
2023-01-14 13:03:55 -0300

    fix: dialect na database

--- src/db/configdb.js
+++ src/db/configdb.js
@@ -4,7 +4,7 @@ import Sequelize from "sequelize";
 dotenv.config();
 
 const database = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
-    dialect: process.env.DB_DIALECT,
+    dialect: "mysql",
     host: process.env.DB_HOST,
     port: process.env.DB_PORT
 })
